b9bfa922a7bb043e29bbe7b52756be1a78f068bc
Rio Balderas <rio@Rios-MacBook-Pro.local>
2016-12-14 11:29:43 -0800

    got chart data working need to resize and work on CSS

--- playground.js
+++ playground.js
@@ -174,47 +174,65 @@ function makeChart () {
     }
     return names;
   };
+
   function renderList() {
  for (var i=0; i < allProducts.length; i++){
    viewed[i] = allProducts[i].views;
    clicked[i] = allProducts[i].clicks;
  }
  }
+
+ renderList();
   var ctx = document.getElementById("myChart");
   var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: myImageNames(),
-          datasets: [{
-              label: ('# of clicks'),
-              data: renderList(),
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }]
-          }
-      }
-  });
+    type: 'bar',
+    data: {
+        labels: myImageNames(),
+        datasets: [{
+            label: ('# of clicks'),
+
+            data: clicked,
+            backgroundColor: [
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy',
+                'navy'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+      height: 200,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
 }
